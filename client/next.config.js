c28169639f3d595b9343e543a3f0768112a6c26f
Ryosuke Yamane <ryosukeyamane17@gmail.com>
2020-08-09 17:03:12 +0900

    chore: Added typescript

---
+++ client/next.config.js
@@ -0,0 +1,22 @@
+const withSourceMaps = require('@zeit/next-source-maps');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+module.exports = withSourceMaps({
+  target: 'serverless',
+  env: {
+    BASE_PATH: process.env.BASE_PATH || 'http://localhost:8080',
+  },
+  webpack: (config, options) => {
+    config.resolve.alias['@fls-lib'] = path.join(__dirname, 'lib');
+    config.resolve.alias['@fls-api-clinet'] = path.join(
+      __dirname,
+      'fls-api-clinet',
+    );
+    config.resolve.alias['@fls-pages'] = path.join(__dirname, 'pages');
+
+    return config;
+  },
+});
