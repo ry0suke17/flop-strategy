f2b600e9dabe64f717b1c04b71d17e04ba5bc78b
Ryosuke Yamane <ryosukeyamane17@gmail.com>
2020-10-13 22:54:13 +0900

    chore: Replaced some data with production data

--- server/_sql/flop_situations.sql
+++ server/_sql/flop_situations.sql
@@ -1,18 +1,11 @@
-INSERT INTO flop_situations VALUES
-  ('8844ae0b-ff5a-47db-8f62-8c818c16f1eb', '6a1e28e1-1d84-446a-a7a6-70e34ffd0d1c', '615b1a9a-0863-4ae0-9f57-0fbcbae47b05'),
-  ('804bc2a4-bc1c-4af8-892e-c66fa745e8c3', '2748ac73-67fe-4e5a-a556-f629ad1f92c8', '615b1a9a-0863-4ae0-9f57-0fbcbae47b05'),
-  ('b4f0dae6-0c7e-48db-b31a-060ac7f6c712', 'f982fe1f-94b7-4dfc-a620-796e4fdff28c', '615b1a9a-0863-4ae0-9f57-0fbcbae47b05'),
-  ('8727d150-1ad2-43bc-a8e6-ee7b122d2c3e', 'b539293e-f0ff-44b2-aa7c-f25d4b6a8cec', '615b1a9a-0863-4ae0-9f57-0fbcbae47b05'),
-  ('f5b27eb1-b765-459c-bbc2-d07f557a2493', 'f352af40-81ae-4807-8f08-3bf02a8842af', '615b1a9a-0863-4ae0-9f57-0fbcbae47b05');
+-- TODO: 本番用に差し替える
+INSERT INTO flop_situations (id, board_id, heads_up_situation_id) VALUES
+('23e079b6-a3a5-4241-97a6-e2b488878a0a', '8a15b4c7-8d2f-4a05-a1bf-ae8c3625b952', '1c7a6fc1-82c0-422a-9feb-13626eba6eda');
 
-INSERT INTO flop_situation_parameters VALUES
-  ('8844ae0b-ff5a-47db-8f62-8c818c16f1eb', 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1),
-  ('804bc2a4-bc1c-4af8-892e-c66fa745e8c3', 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1),
-  ('b4f0dae6-0c7e-48db-b31a-060ac7f6c712', 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1),
-  ('8727d150-1ad2-43bc-a8e6-ee7b122d2c3e', 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1),
-  ('f5b27eb1-b765-459c-bbc2-d07f557a2493', 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1);
+-- TODO: 本番用に差し替える
+INSERT INTO flop_situation_parameters (flop_situation_id, out_of_position_bet_frequency, in_position_bet_frequency, out_of_position_check_frequency, in_position_check_frequency, out_of_position_equity, in_position_equity, out_of_position_33_bet_frequency, in_position_33_bet_frequency, out_of_position_67_bet_frequency, in_position_67_bet_frequency) VALUES
+('23e079b6-a3a5-4241-97a6-e2b488878a0a', 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1);
 
-INSERT INTO flop_situation_images VALUES
-  ('8844ae0b-ff5a-47db-8f62-8c818c16f1eb', 'https://user-images.githubusercontent.com/17821088/83936722-41c3b500-a801-11ea-8ca4-ea8db40efa6a.png', 'これはメモです。これはメモです。これはメモです。これはメモです。これはメモです。'),
-  ('8727d150-1ad2-43bc-a8e6-ee7b122d2c3e', 'https://user-images.githubusercontent.com/17821088/83936733-5c962980-a801-11ea-843c-fd1aa51ebf64.png', 'これはメモです。これはメモです。これはメモです。これはメモです。これはメモです。'),
-  ('f5b27eb1-b765-459c-bbc2-d07f557a2493', 'https://user-images.githubusercontent.com/17821088/83936733-5c962980-a801-11ea-843c-fd1aa51ebf64.png', 'これはメモです。これはメモです。これはメモです。これはメモです。これはメモです。');
+-- TODO: 本番用に差し替える
+INSERT INTO flop_situation_images (flop_situation_id, url, description) VALUES
+('23e079b6-a3a5-4241-97a6-e2b488878a0a', 'https://shura-poker.com/wp-content/uploads/2018/10/6max-utg.jpg', 'This is a description.');
